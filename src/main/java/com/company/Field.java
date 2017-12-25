026751fab116392f1b5c45c6a47a50a9c55ee43b
Mikhail <maiklbsn@yandex.ru>
2017-12-25 17:14:10 +0300

    no message

--- src/main/java/com/company/Field.java
+++ src/main/java/com/company/Field.java
@@ -1,170 +1,269 @@
 package com.company;
 
-
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.List;
+import java.util.*;
 
 public class Field {
-    private char [][] field;
-    private int [][]graph;
-    private int start_vert,finish_vert,start_port,finish_port,size;
+    private int mountain = -1, noPoint = -1;
+    private int countOfStartPoints = 0, getCountOfFinishPoints = 0;
+    private char[][] field;
+    private int[][] graph;
+    private Vector<Integer> ports = new Vector();
+    private int start_vert = noPoint, finish_vert = noPoint, start_port, finish_port, size, size_matrix;
+    private List<String> fieldFromJson = new ArrayList();
 
+    public Field(JsonField text) {
 
-    public Field(char []matrix) {
-        int size_matrix=(int)Math.sqrt(matrix.length);
-        field=new char[size_matrix][size_matrix];
-        int elem=0;
-        for(int i=0;i<size_matrix;i++){
-            for(int j=0;j<size_matrix;j++){
-                field[i][j]=matrix[elem];
-                elem++;
+        fieldFromJson=text.getField();
+        size_matrix=fieldFromJson.size();
+        if (size_matrix > 20) {
+            throw new StackOverflowError("Input data is too big. Size was: " + size_matrix);
+        }
+        for (int i = 0; i <fieldFromJson.size() ; i++) {
+            if(fieldFromJson.get(i).length()!=fieldFromJson.size())
+                throw new EmptyStackException();
+        }
+            field=new char [size_matrix][size_matrix];
+        for (int i = 0; i <size_matrix; i++) {
+            for (int j = 0; j <size_matrix; j++) {
+                field[i][j]=fieldFromJson.get(i).charAt(j);
             }
         }
 
-        int [][]field2=new int[field.length][field[0].length];
-        int flag =0;
-        int count_vertex=0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (field[i][j] == 'S') {
-                    start_vert=count_vertex;
 
+        int[][] field2 = new int[size_matrix][size_matrix];
+        int count_vertex = 0;
+        for (int i = 0; i < size_matrix; i++) {
+            for (int j = 0; j < size_matrix; j++) {
+                if (isStart(i, j)) {
+                    start_vert = count_vertex;
+                    countOfStartPoints++;
                 }
-                if (field[i][j] == 'F') {
-
-                    finish_vert=count_vertex;
-
+                if (isFinish(i, j)) {
+                    finish_vert = count_vertex;
+                    getCountOfFinishPoints++;
                 }
-                if(field[i][j]=='P' && flag==0){
-
-                    flag =1;
-                    start_port=count_vertex;
-
+                if (isPort(i, j)) {
+                    ports.add(count_vertex);
                 }
-                if(field[i][j]=='P' && flag==1){
-                    finish_port=count_vertex;
 
-
-                }
-                if(field[i][j]=='^') {
-                    field2[i][j]=-1;
+                if (isMountain(i, j)) {
+                    field2[i][j] = -1;
                     count_vertex--;
-                }
-                else
-                    field2[i][j]=count_vertex;
+                } else
+                    field2[i][j] = count_vertex;
                 count_vertex++;
 
             }
         }
+        if (ports.size() == 0 || start_vert == noPoint || finish_vert == noPoint) {
+            throw new NoSuchElementException();
+        }
+        if (countOfStartPoints > 1 || getCountOfFinishPoints > 1) {
+            throw new StackOverflowError();
+        }
+
+
 //
 //
 //
-        size=count_vertex;
-        graph=new int[count_vertex][4];
+        size = count_vertex;
+        graph = new int[count_vertex][4];
 //
 //
 
 //
 //      Graph initialization
 //
-        int count=0;
-        for(int i=0;i<field2.length;i++){
-            for(int j=0;j<field2[i].length;j++){
-                if(field2[i][j]!=-1){
-                    if((i-1)>=0 ) {
-                        if(field2[i-1][j]!=-1) {
-                            graph[field2[i][j]][count] = field2[i -1][j];
+        int count = 0;
+        for (int i = 0; i < size_matrix; i++) {
+            for (int j = 0; j < size_matrix; j++) {
+                if (field2[i][j] != mountain) {
+                    if (leftTopIndexInsideTheMatrix(i)) {
+                        if (field2[i - 1][j] != mountain) {
+                            graph[field2[i][j]][count] = field2[i - 1][j];
                             count++;
                         }
                     }
 
-                    if((j-1)>=0)  {
-                        if(field2[i][j-1]!=-1) {
+                    if (leftTopIndexInsideTheMatrix(j)) {
+                        if (field2[i][j - 1] != mountain) {
                             graph[field2[i][j]][count] = field2[i][j - 1];
                             count++;
                         }
                     }
 
-                    if((j+1)<=3){
-                        if(field2[i][j+1]!=-1) {
+                    if (rightBottomIndexInsideTheMatrix(j)) {
+                        if (field2[i][j + 1] != mountain) {
                             graph[field2[i][j]][count] = field2[i][j + 1];
                             count++;
                         }
                     }
 
-                    if((i+1)<=3){
-                        if(field2[i+1][j]!=-1)
-                            graph[field2[i][j]][count]=field2[i+1][j];
+                    if (rightBottomIndexInsideTheMatrix(i)) {
+                        if (field2[i + 1][j] != mountain) {
+                            graph[field2[i][j]][count] = field2[i + 1][j];
+                            count++;
+                        }
 
                     }
-                    count=0;
+                    if (count == 0)
+                        graph[field2[i][j]][count] = -1;
+                    count = 0;
 
                 }
             }
         }
-
     }
 
-
-    public int GetWay(int from,int to){
+    public int GetWay(int from, int to) {
         Queue<Integer> queue = new LinkedList();
         queue.add(from);
-        boolean []used=new boolean[size];
-        int []p=new int[size];
-        for(int i=0;i<size;i++){
-            used[i]=false;
-            p[i]=-1;
+        boolean[] used = new boolean[size];
+        int[] p = new int[size];
+        for (int i = 0; i < size; i++) {
+            used[i] = false;
+            p[i] = -1;
         }
-        used[from]=true;
-        while(!queue.isEmpty()){
-            int v=queue.poll();
-            for(int i=0;i<4;i++){
-                if(graph[v][i]>0 || (graph[v][i]==0 && i==0)){
-                    if(!used[graph[v][i]]){
-                        used[graph[v][i]]=true;
+        used[from] = true;
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                if (graph[v][i] > 0 || (graph[v][i] == 0 && i == 0)) {
+                    if (!used[graph[v][i]]) {
+                        used[graph[v][i]] = true;
                         queue.add(graph[v][i]);
-                        p[graph[v][i]]=v;
+                        p[graph[v][i]] = v;
                     }
                 }
             }
-
         }
 
-        int path=0;
-        for(int v=to;v!=-1;v=p[v])
-        {
-
+        int path = 0;
+        for (int v = to; v != -1; v = p[v]) {
             path++;
         }
-        return (path-1)*2;
 
-
-
-
+        return (path - 1) * 2;
     }
 
-    public int FasterWay(){
-        if(GetWay(start_vert,start_port)>GetWay(start_vert,finish_port)){
-            int tmp=start_port;
-            start_port=finish_port;
-            finish_port=tmp;
-        }
-        else if(GetWay(start_vert,start_port)==GetWay(start_vert,finish_port)){
-            if(GetWay(start_port,finish_vert)<GetWay(finish_port,finish_vert)){
-                int tmp=start_port;
-                start_port=finish_port;
-                finish_port=tmp;
+    public int fasterWay() {
+
+        Vector<Integer> start_ports = new Vector();
+
+        int min = size_matrix * size_matrix * 2;
+        for (int i = 0; i < ports.size(); i++) {
+            int portElement = (int) ports.elementAt(i);
+            int way = GetWay(start_vert, portElement);
+            if (way < min) {
+                start_ports.clear();
+                start_ports.add(portElement);
+                min = way;
+            } else if (way == min) {
+                start_ports.add(portElement);
             }
         }
-        int earth_path=GetWay(start_vert,finish_vert);
-        int sea_path=GetWay(start_vert,start_port)+(GetWay(start_port,finish_port)/2+2)+GetWay(finish_port,finish_vert);
 
-        if(earth_path>=sea_path)
+        //
+        // find optimal start_port
+        //
+        start_port = (int) start_ports.firstElement();
+        min = GetWay(start_port, finish_vert);
+        for (int i = 1; i < start_ports.size(); i++) {
+            int way = GetWay((int) start_ports.elementAt(i), finish_vert);
+            if (way < min) {
+                min = way;
+                start_port = (int) start_ports.elementAt(i);
+            }
+        }
+        ports.removeElement(start_port);
+//
+//      find optimal finish_ports
+//
+        min = size_matrix * size_matrix * 2;
+        for (int i = 0; i < ports.size(); i++) {
+            int portElement = (int) ports.elementAt(i);
+            int way = GetWay(start_port, portElement) / 2 + 2 + GetWay(portElement, finish_vert);
+            if (way < min) {
+                finish_port = portElement;
+                min = way;
+            }
+
+        }
+
+
+        if (GetWay(start_vert, start_port) > GetWay(start_vert, finish_port)) {
+            int tmp = start_port;
+            start_port = finish_port;
+            finish_port = tmp;
+        } else if (GetWay(start_vert, start_port) == GetWay(start_vert, finish_port)) {
+            if (GetWay(start_port, finish_vert) < GetWay(finish_port, finish_vert)) {
+                int tmp = start_port;
+                start_port = finish_port;
+                finish_port = tmp;
+            }
+        }
+        int earth_path = GetWay(start_vert, finish_vert);
+        int sea_path;
+
+
+        int btwn_strt_sport = GetWay(start_vert, start_port);
+        int btwn_ports = GetWay(start_port, finish_port);
+        if (btwn_ports != 0)
+            btwn_ports = btwn_ports / 2 + 2;
+        int btwn_fport_finish = GetWay(finish_port, finish_vert);
+        if (btwn_strt_sport == 0 || btwn_ports == 0)
+            sea_path = 0;
+        else
+            sea_path = btwn_strt_sport + btwn_ports + btwn_fport_finish;
+
+
+        if (earth_path == 0)
+            return 0;
+        else if (earth_path >= sea_path && sea_path != 0)
             return sea_path;
         else
             return earth_path;
+
+    }
+
+    boolean isStart(int i, int j) {
+        if ((field[i][j] == 'S') || (field[i][j] == 's'))
+            return true;
+        return false;
+    }
+
+    boolean isFinish(int i, int j) {
+        if ((field[i][j] == 'F') || (field[i][j] == 'f'))
+            return true;
+        return false;
+    }
+
+    boolean isPort(int i, int j) {
+        if ((field[i][j] == 'P') || (field[i][j] == 'p'))
+            return true;
+        return false;
+    }
+
+    boolean isMountain(int i, int j) {
+        if (field[i][j] == '^')
+            return true;
+        return false;
+    }
+
+    boolean leftTopIndexInsideTheMatrix(int i) {
+        if (i - 1 >= 0)
+            return true;
+        return false;
+    }
+
+    boolean rightBottomIndexInsideTheMatrix(int i) {
+        if (i + 1 <= size_matrix - 1)
+            return true;
+        return false;
     }
 
 
 }
+
 
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,16 +1,30 @@
 package com.company;
 
-
+import org.codehaus.jackson.map.ObjectMapper;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class Main {
 
 
     public static void main(String[] args) {
-        char []test={'*','*','^','F' ,'P','*','*','P','*','*','*','*','S','*','*','*'};
-        Field f = new Field(test);
-        int path=f.FasterWay();
-        System.out.println("Faster path: "+path);
+
+    ObjectMapper mapper = new ObjectMapper();
+    InputStream inputStream = JsonField.class.getClassLoader().getResourceAsStream("text.json");
+    try {
+            JsonField text=(JsonField) mapper.readValue(inputStream,JsonField.class);
+            text.out();
+            Field f=new Field(text);
+            System.out.println(f.fasterWay());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
+
+
 }
 
